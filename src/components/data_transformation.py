2f6f269e6ca579fe542e1a98d8de54a24dfe4d58
Abhilash Jaiswal <abhilashaj2503@gmail.com>
2026-07-12 14:26:53 +0530

    main

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -112,10 +112,10 @@ class DataTransformation:
 
 
             # all data ( train + test ) has to be transformed using the same preprocessor object, 
-            input_feature_train_df=train_df.drop(columns=[target_column_name],axis=1)
+            input_feature_train_df=train_df.drop(columns=[target_column_name])
             target_feature_train_df=train_df[target_column_name]
 
-            input_feature_test_df=test_df.drop(columns=[target_column_name],axis=1)
+            input_feature_test_df=test_df.drop(columns=[target_column_name])
             target_feature_test_df=test_df[target_column_name]
 
             logging.info( f"Applying preprocessing object on training dataframe and testing dataframe."  )
